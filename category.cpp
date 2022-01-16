4ab1cd3a8b87b22186538498ecb6d30ddf7c3bf6
ryn5 <ryanmtan5@gmail.com>
2022-01-15 22:41:37 -0800

    Implement static findCat for categories

--- category.cpp
+++ category.cpp
@@ -5,3 +5,10 @@ vector<Category> Category::cats;
 Category::Category() {};
 
 Category::Category(string name) : name(name) {};
+
+Category Category::findCat(string name) {
+    for (Category & c : Category::cats) {
+        if (c.name == name) return c;
+    }
+    return Category();
+}
--- category.h
+++ category.h
@@ -17,6 +17,7 @@ public:
     Category();
     Category(string name);
 
+    static Category findCat(string name);
 };
 
 #endif // CATEGORY_H
