89f4173c9d37bc9b2122b68bf0331d613bf9eafe
ryn5 <ryanmtan5@gmail.com>
2022-01-15 19:11:45 -0800

    Make category name public

--- category.cpp
+++ category.cpp
@@ -4,11 +4,7 @@ vector<Category> Category::cats;
 
 Category::Category() {};
 
-Category::Category(string cat_name) : cat_name(cat_name)
+Category::Category(string name) : name(name)
 {
     cats.push_back(*this);
-}
-
-string Category::name() {
-    return cat_name;
 }
