63d8ada5df6c6d0fcf99d682a0611996ac4bb2d2
ryn5 <ryanmtan5@gmail.com>
2022-01-15 22:42:40 -0800

    Create add event dialogue
    Implementation almost complete.  Needs to add QDate member to day class in order to find and add itself to a day.

---
+++ eventdialog.h
@@ -0,0 +1,27 @@
+#ifndef EVENTDIALOG_H
+#define EVENTDIALOG_H
+
+#include <QDialog>
+
+namespace Ui {
+class EventDialog;
+}
+
+class EventDialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit EventDialog(QWidget *parent = nullptr);
+    ~EventDialog();
+
+private slots:
+    void on_addButton_clicked();
+
+    void on_cancelButton_clicked();
+
+private:
+    Ui::EventDialog *ui;
+};
+
+#endif // EVENTDIALOG_H
