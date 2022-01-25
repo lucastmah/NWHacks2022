41df4424592f717d5cf9633dde731c82da50750c
ryn5 <ryanmtan5@gmail.com>
2022-01-24 21:47:16 -0800

    Update Javadoc for category.cpp

--- category.cpp
+++ category.cpp
@@ -7,9 +7,9 @@ Category::Category() {};
 Category::Category(string name) : name(name) {};
 
 /**
- * @brief Category::findCat
- * @param name
- * @return
+ * @brief Category::findCat returns an existing category with the given name
+ * @param name that should match an existing category
+ * @return category with given name
  */
 Category Category::findCat(string name) {
     for (Category & c : Category::cats) {
